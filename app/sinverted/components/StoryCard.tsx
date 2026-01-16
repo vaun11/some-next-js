f1781f681f7fca79ad41416775a248ef068dd6f8
vaun11 <mvaun@proton.me>
2026-01-16 16:02:43 -0500

    wip: laying out a main article page

---
+++ app/sinverted/components/StoryCard.tsx
@@ -0,0 +1,13 @@
+export function StoryCard(){ 
+    return (
+        <div className="flex min-h-[350px] bg-blue-500 max-w-[350px] overflow-hidden">
+            <p>
+                This is some text
+
+                <br/>
+
+                This is some more text
+            </p>
+        </div>
+    )
+}
--- app/sinverted/page.tsx
+++ app/sinverted/page.tsx
@@ -1,9 +1,23 @@
+import { Children, ReactNode } from "react";
 import { TopNavBar } from "./components/TopNavBar";
+import { StoryCard } from "./components/StoryCard";
 
 export default function SinvertedPage(){
     return (
         <div className="flex flex-col min-h-screen bg-black">
             <TopNavBar/>
+            <StoryCardContainer>
+                <StoryCard/>
+                <StoryCard/>
+            </StoryCardContainer>
+        </div>
+    )
+}
+
+function StoryCardContainer(props: {children: ReactNode}) {
+    return (
+        <div className="flex flex-row bg-purple-500 overflow-hidden gap-3"> 
+            {props.children}
         </div>
     )
 }
