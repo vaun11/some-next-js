8b81108c23331543296abe74b9dd95958d5eaecd
vaun11 <mvaun@proton.me>
2026-01-16 15:39:06 -0500

    wip: component refactor

---
+++ app/sinverted/components/TopNavBar.tsx
@@ -0,0 +1,10 @@
+export function TopNavBar(){
+
+    return (
+        <div className="flex flex-row h-16 bg-[#ff00aa] items-center gap-2">
+            <a href="/" className="flex text-white font-bold italic">sinverted.com</a>    
+            <a className="flex text-white">Home</a>
+            <a className="flex text-white">Test</a>
+        </div>   
+    )
+}
--- app/sinverted/page.tsx
+++ app/sinverted/page.tsx
@@ -1,11 +1,10 @@
+import { TopNavBar } from "./components/TopNavBar";
+
 export default function SinvertedPage(){
     return (
         <div className="flex flex-col min-h-screen bg-black">
-            <div className="flex flex-row h-16 bg-[#ff00aa] items-center gap-2">
-                <a href="/" className="flex text-white font-bold italic">sinverted.com</a>    
-                <a className="flex text-white">Home</a>
-                <a className="flex text-white">Test</a>
-            </div>        
+            <TopNavBar/>
         </div>
     )
 }
+
