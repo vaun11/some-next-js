12581c30905b6633356c9d21990c0ffb54f78369
vaun11 <mvaun@proton.me>
2026-01-16 15:37:36 -0500

    wip: coloring, styling, etc

--- app/sinverted/page.tsx
+++ app/sinverted/page.tsx
@@ -1,8 +1,8 @@
 export default function SinvertedPage(){
     return (
         <div className="flex flex-col min-h-screen bg-black">
-            <div className="flex flex-row h-16 bg-purple-500 items-center gap-2">
-                <a href="/" className="flex text-white font-bold italic">SINVERTED.com</a>    
+            <div className="flex flex-row h-16 bg-[#ff00aa] items-center gap-2">
+                <a href="/" className="flex text-white font-bold italic">sinverted.com</a>    
                 <a className="flex text-white">Home</a>
                 <a className="flex text-white">Test</a>
             </div>        
