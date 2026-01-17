fcf535f80e4842eee73f9951da74b93441c52172
vaun11 <mvaun@proton.me>
2026-01-16 22:27:58 -0500

    wip: add route for change quote

---
+++ app/change/page.tsx
@@ -0,0 +1,26 @@
+export default function ChangeRoute() {
+    return (
+        <div className="min-h-screen flex justify-center">
+            <div className="flex flex-col w-64 justify-center items-center gap-4">
+                <div className="flex flex-row justify-center align-center w-32">
+                    <div className="flex text-center italic font-bold">Change, and everything is change</div>
+                </div>
+                <div className="flex flex-row justify-center align-center">
+                    <div className="flex text-center">To the degree you go with the stream, you see you are still, you are flowing with it.</div>
+                </div>
+
+                <div className="flex flex-row justify-center align-center">
+                    <div className="flex text-center">But to the degree you resist the stream, you see the current is rushing past you and fighting you.</div>
+                </div>
+
+                <div className="flex flex-col gap-4 italic w-32 justify-center align-center text-center">
+                    <div className="">So swim with it.</div>
+                    <div className="">Go with it.</div>
+                    <div className="">And you are there...</div>
+                    <div className="font-bold">You are at rest.</div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
