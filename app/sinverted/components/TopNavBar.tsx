87ca8811ea8a003cfdad43342bfc25efcb795bf3
vaun11 <mvaun@proton.me>
2026-01-17 11:34:02 -0500

    fix: tweak color of navbar on sinverted

--- app/sinverted/components/TopNavBar.tsx
+++ app/sinverted/components/TopNavBar.tsx
@@ -1,7 +1,7 @@
 export function TopNavBar(){
 
     return (
-        <div className="flex flex-row h-16 bg-[#ff00aa] items-center gap-2">
+        <div className="flex flex-row h-16 bg-[#ff0077] items-center gap-2">
             <a href="/" className="flex text-white font-bold italic">sinverted.com</a>    
             <a className="flex text-white">Home</a>
             <a className="flex text-white">Test</a>
