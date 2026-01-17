47c076cb06bd2f1946fa1df9ec6a138a9c45f27b
vaun11 <mvaun@proton.me>
2026-01-16 23:01:37 -0500

    wip: more things happening

---
+++ app/change/deeper/page.tsx
@@ -0,0 +1,26 @@
+export default function LevelDeeper(){
+    return (
+        <div className="min-h-screen flex justify-center">
+            <div className="flex flex-col w-[500px] pt-10">
+                <div className="flex py-2">Going deeper, we see that the present and future are connected to the past</div>
+                <div className="flex py-2">However, they are not connected by a fixed linear view of this chain of events</div>
+                <div className="flex py-2">Stemming outwards from the present lies the future. If we assume it from what we know of the present moment, what do we learn?</div>
+                <div className="flex py-2">We see that there are a web of possibilities, a spectrum of variations that can extend in nearly any direction</div>
+                <div className="flex py-2">We can theorize, idealize, and enact action that takes us closer to our personal desired states inside that spectrum, although it is murky, indirect</div>
+                <div className="flex py-2">One can liken it to moving along a foggy forest path.</div>
+                <div className="flex py-2">Yet behind you, if that were the allerogical past, is also foggy</div>
+                <div className="flex py-2">Perfect recollection of the past is not directly possible, as it always relies on your current state: the present moment. You will view your memories, past, and prior paths through the lens of your current state of mind and body.</div>
+                <div className="flex py-2"><p>What is further interesting, is the fact that the past is composed of many moments, integrally, that used to be the present. And the future is composed of many moments, integrally, that <strong>will</strong> be the present</p></div>
+                <br/>
+                <div className="h-[2px] w-full bg-black"/>
+                <div className="flex py-2"><p>Thus, the present is the always changing context of the past and future. Where the past and future walk hand in hand, two arms of infinity stretching outwards in seemingly opposite directions</p></div>
+
+                <div className="flex py-2"><p>However, this is where things truly do get tricky. Because seeing the past as stretching backwards as a single vector towards... what exactly? The big bang? God's creation story? Either way... it gets tricky. Because there is increasing uncertainty the further you go back</p></div>
+                <div className="flex py-2"><p>And the same things happen when you go towards the future. What do you march towards? Your death, untimely or not? The legacy of humanity and civilization {`(positive or negative outcome)`}? Or if you go far enough out... then what? Is it the "Big Crunch" as some theoeretical physicists theorize it to be?</p></div>
+                <br/>
+                <div className="h-[2px] w-full bg-black"/>
+
+            </div>
+        </div>
+    )
+}
