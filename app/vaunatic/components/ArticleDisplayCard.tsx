faa075315d8b34bf32e86ca79e1c0a4be8f9c1dc
vaun11 <mvaun@proton.me>
2026-01-17 21:54:22 -0500

    fix: tweak height of title of article cards

--- app/vaunatic/components/ArticleDisplayCard.tsx
+++ app/vaunatic/components/ArticleDisplayCard.tsx
@@ -9,11 +9,11 @@ export default function ArticleDisplayCard({props}: {props: ArticleDisplayCardPr
     const cardColorTw = props.color == "red" ? "bg-red-500" : props.color == "blue" ? "bg-blue-500" : "bg-purple-500"
 
     return (
-        <div className="flex flex-col max-w-[400px] md:w-full w-72 h-[400px]">
+        <div className="flex flex-col max-w-[400px] md:w-full w-72 h-[450px]">
                 <div className={`flex h-full w-full ${cardColorTw} justify-center`}>
                     <p>{props.caption}</p>
                 </div>
-                <div className="flex">
+                <div className="flex h-24">
                     <h2>{props.title}</h2>
                 </div>
             </div>
