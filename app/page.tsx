08be67bd8157b744d3891f0595cbed5411a9e4d5
vaun11 <mvaun@proton.me>
2026-01-18 21:13:07 -0500

    chore: whack dead code, redirect, etc

--- app/page.tsx
+++ app/page.tsx
@@ -41,13 +41,13 @@ export default function Home() {
         </a>
 
         <a
-          href="/sinverted"
+          href="/vaunatic"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target=""
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Sinverted Homepage{' '}
+            Vaunatic Homepage{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
