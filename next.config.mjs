2e574b1ecda57ba3cb8acae360269a0a282fd679
vaun11 <mvaun@proton.me>
2026-01-18 21:44:47 -0500

    fix: package/config/dev stuff

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@ import createMDX from "@next/mdx"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx']
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+    reactStrictMode: true
 };
 
 const withMDX = createMDX({
